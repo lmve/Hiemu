a19d4e6515d569a0e10940d7c48b898a6dc2d714
lmve <1831232566@qq.com>
2025-05-13 23:57:07 +0800

    Implement the loading of all segments from the ELF file.       src/Hiemu.c       src/Hiemu.h       src/mmu.c
    Signed-off-by: lmve <1831232566@qq.com>

--- src/Hiemu.c
+++ src/Hiemu.c
@@ -5,5 +5,7 @@ int main(int argc, char *argv[]) {
     machine_t machine = {0};
     machine_load_program(&machine, argv[1]);
     printf("Entry: %lx\n", machine.mmu.entry);
+    printf("machine address: 0x%lx\n", (u64)&machine);
+    printf("Entry: 0x%llx\n", TO_HOST(machine.mmu.entry));
     return 0;
 }
--- src/mmu.c
+++ src/mmu.c
@@ -1,5 +1,47 @@
 #include "Hiemu.h"
 
+static void load_phdr(elf64_phdr_t *phdr, elf64_ehdr_t *ehdr, i64 i, FILE *file) {
+    if(fseek(file, ehdr->e_phoff + i * ehdr->e_phentsize, SEEK_SET) != 0) {
+        fatal("seek file failed.");
+    }
+    
+    if(fread((void *)phdr, 1, sizeof(elf64_phdr_t), file) != sizeof(elf64_phdr_t)) {
+        fatal("read file too small.");
+    }
+
+}
+static int flags_to_mmap_port(u32 flags) {
+    return (flags & PF_R ? PROT_READ : 0) |
+           (flags & PF_W ? PROT_WRITE : 0) |
+           (flags & PF_X ? PROT_EXEC : 0);
+}
+static void mmu_load_segment(mmu_t *mmu, elf64_phdr_t *phdr, int fd) {
+    int page_size = getpagesize();
+    u64 offset = phdr->p_offset;
+    u64 vaddr = TO_HOST(phdr->p_vaddr);
+    u64 aligned_vaddr = ROUNDDOWN(vaddr, page_size);
+    u64 file_size = phdr->p_filesz + (vaddr - aligned_vaddr);
+    u64 memsz = phdr->p_memsz + (vaddr - aligned_vaddr);
+    int prot = flags_to_mmap_port(phdr->p_flags);
+
+    u64 addr = (u64)mmap((void *)aligned_vaddr, file_size, prot, MAP_PRIVATE | MAP_FIXED,
+                          fd, ROUNDDOWN(offset, page_size));
+    assert(addr == aligned_vaddr);
+
+    u64 remaining_bss = ROUNDDOWN(memsz, page_size) - ROUNDDOWN(file_size, page_size);
+    if(remaining_bss > 0) {
+        u64 addr = (u64)mmap((void*)(aligned_vaddr + ROUNDDOWN(file_size, page_size)),
+                                     remaining_bss, prot, MAP_ANONYMOUS | MAP_PRIVATE | MAP_FIXED, -1, 0);
+
+        assert(addr == aligned_vaddr + ROUNDDOWN(file_size, page_size));
+    }
+
+    mmu->host_alloc = MAX(mmu->host_alloc, (aligned_vaddr + ROUNDDOWN(memsz, page_size)));
+
+    mmu->base = mmu->alloc = TO_GUST(mmu->host_alloc);
+    //      [      ELF        | malloc      ]
+    // host [      ELF        | host_alloc  |  ...]
+}
 void mmu_load_elf(mmu_t *mmu, int fd) {
     u8 buf[sizeof(elf64_ehdr_t)];
     FILE *file = fdopen(fd, "rb");
@@ -17,4 +59,13 @@ void mmu_load_elf(mmu_t *mmu, int fd) {
         fatal("Only support ELF64 and RISC-V.");
     }
     mmu->entry = (u64)ehdr->e_entry;
+
+    elf64_phdr_t phdr;
+    for(i64 i = 0; i < ehdr->e_phnum; i++) {
+        load_phdr(&phdr, ehdr, i, file);
+
+        if(phdr.p_type == PT_LOAD) {
+            mmu_load_segment(mmu, &phdr, fd);
+        }
+    }
 }
